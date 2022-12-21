b637869bc44085986dd3387cb8a162f75bcc4bfa
giorgip62 <giorgiop62@gmail.com>
2022-12-21 16:04:25 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="{{asset('css/style.css')}}">
+  <title>Laravel-primi-passi</title>
+</head>
+<body>
+  <div class="container">
+    <h1>{{$hello}}</h1>
+
+    <a href="/info">Info</a>
+
+  </div>
+
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+
+    $hello = 'Ciao Laravel';
+
+
+    return view('home', compact('hello'));
+});
+
+Route::get('/info',function(){
+    $help = 'sarà difficile?';
+
+    return view('info',compact('help'));
+
+});
